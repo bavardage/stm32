188f9182514b7ba31857017c0d614690111def18
Ben Duffield <bduffield@Bens-MacBook-Pro.local>
2022-11-11 11:30:44 -0500

    move to 144 pin board

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
 #![no_std]
 #![no_main]
 
-use core::cell::RefCell;
-
-use hal::{
-    gpio::{Gpiob, Gpioc, Pin, U},
-    interrupt,
-};
-
 // pick a panicking behavior
 // use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
 // use panic_abort as _; // requires nightly
@@ -15,92 +8,64 @@ use hal::{
 use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 
 use core::fmt::Write;
-use cortex_m::{
-    asm,
-    interrupt::{CriticalSection, Mutex},
-    peripheral::NVIC,
-};
 use cortex_m_rt::{entry, exception};
 use cortex_m_semihosting::hio;
-use cortex_m_semihosting::hprintln;
 
-use stm32f3xx_hal::{
-    self as hal,
-    gpio::{Edge, Input, Output, PushPull},
+use stm32h7xx_hal::{
     pac,
     prelude::*,
 };
 
-type LedPin = Pin<Gpiob, U<13>, Output<PushPull>>;
-static LED: Mutex<RefCell<Option<LedPin>>> = Mutex::new(RefCell::new(None));
-type ButtonPin = Pin<Gpioc, U<13>, Input>;
-static BUTTON: Mutex<RefCell<Option<ButtonPin>>> = Mutex::new(RefCell::new(None));
-
 #[entry]
 fn main() -> ! {
+    let cp = cortex_m::Peripherals::take().unwrap();
     let dp = pac::Peripherals::take().unwrap();
-    let mut rcc = dp.RCC.constrain();
-    let mut syscfg = dp.SYSCFG.constrain(&mut rcc.apb2);
-    let mut exti = dp.EXTI;
 
-    // grab the led
-    let mut gpiob = dp.GPIOB.split(&mut rcc.ahb);
-    let led: Pin<Gpiob, U<13>, Output<PushPull>> = gpiob
-        .pb13
-        .into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper);
+    let pwr = dp.PWR.constrain();
+    let pwrcfg = pwr.freeze();
 
-    // store it so we can share with interrupt
-    cortex_m::interrupt::free(|cs| *LED.borrow(cs).borrow_mut() = Some(led));
+    let rcc = dp.RCC.constrain();
+    let ccdr = rcc.sys_ck(100.MHz()).freeze(pwrcfg, &dp.SYSCFG);
 
-    // grab the button
-    let mut gpioc = dp.GPIOC.split(&mut rcc.ahb);
-    let mut button = gpioc
-        .pc13
-        .into_pull_down_input(&mut gpioc.moder, &mut gpioc.pupdr);
-    syscfg.select_exti_interrupt_source(&button);
-    button.trigger_on_edge(&mut exti, Edge::Rising);
-    button.enable_interrupt(&mut exti);
-    let button_interrupt_num = button.interrupt();
 
-    // store it off
-    cortex_m::interrupt::free(|cs| *BUTTON.borrow(cs).borrow_mut() = Some(button));
-    unsafe { NVIC::unmask(button_interrupt_num) };
+    // grab the leds
+    let gpiob = dp.GPIOB.split(ccdr.peripheral.GPIOB);
+    let gpioe = dp.GPIOE.split(ccdr.peripheral.GPIOE);
 
-    let delay = 1_000_000;
+    let mut led1 = gpiob
+        .pb0
+        .into_push_pull_output();
+    let mut led2 = gpioe.pe1.into_push_pull_output();
+    let mut led3 = gpiob.pb14.into_push_pull_output();
+
+    let mut delay = cp.SYST.delay(ccdr.clocks);
+
+    let mut state: u8 = 0;
 
     loop {
-        hprintln!("loop!").unwrap();
-        cortex_m::interrupt::free(toggle_led);
-        asm::delay(delay);
+        // hprintln!("loop! state: {:?}", state).unwrap();
+        delay.delay_ms(125_u16);
+
+        if state & 0x1 != 0 {
+            led1.set_high();
+        } else {
+            led1.set_low();
+        }
+
+        if state & 0x2 != 0 {
+            led2.set_high();
+        } else {
+            led2.set_low();
+        }
+
+        if state & 0x4 != 0 {
+            led3.set_high();
+        } else {
+            led3.set_low();
+        }
+
+        state = (state + 1) % 8;
     }
-}
-
-fn toggle_led(cs: &CriticalSection) {
-    LED.borrow(cs)
-        .borrow_mut()
-        .as_mut()
-        .unwrap()
-        .toggle()
-        .unwrap()
-}
-
-fn toggle_led_from_interrupt(cs: &CriticalSection) {
-    toggle_led(cs);
-}
-
-#[interrupt]
-fn EXTI15_10() {
-    cortex_m::interrupt::free(|cs| {
-        toggle_led_from_interrupt(cs);
-
-        // Clear the interrupt pending bit so we don't infinitely call this routine
-        BUTTON
-            .borrow(cs)
-            .borrow_mut()
-            .as_mut()
-            .unwrap()
-            .clear_interrupt();
-    })
 }
 
 #[exception]
